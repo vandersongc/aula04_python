4af20792edc777f0bf65f246c7f6744ead9fe694
vandersongc <vandersongc@gmail.com>
2025-10-21 19:17:58 -0300

    v1 211025

--- exercicio_calculadora.py
+++ exercicio_calculadora.py
@@ -7,12 +7,16 @@ def calculadora():
     
     while True:
         try:
+            print('Calculadora Python')
+            print('='*30)
             num1 = int(input('Digite o 1º número: '))
             operador = input("Digite o operador (+, -, *, /): ")
             num2 = int(input('Digite o 2º número: '))
+            print('='*30)
         except ValueError:
             print('Entre com algum valor interior e/ou use somente os operadores apropriados.')
-   
+            print('='*30)
+            break
 
         # Realiza o cálculo baseado no operador
         if operador == '+':
@@ -29,11 +33,12 @@ def calculadora():
         # Tratamento para evitar divisão por zero
             if num2 == 0:
                 print("Erro: Não é possível dividir por zero.")
-            else:
+            elif num2 > 0:
                 resultado = num1 / num2
                 return resultado
             else:
                 print('Operação não reconhecida.')
 
-print('O resultado é:')
+
+
 print(calculadora())
