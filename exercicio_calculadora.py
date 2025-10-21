fcc6fa1e27cd30ad58af8a1be4b3ba81436a2e1d
vandersongc <vandersongc@gmail.com>
2025-10-20 21:08:38 -0300

    v5

--- exercicio_calculadora.py
+++ exercicio_calculadora.py
@@ -1,32 +1,39 @@
+'''
+​1- Criar uma calculadora utilizando funções onde o usuário terá que inserir dois valores e o operador aritmético.​
+
+'''
+
 def calculadora():
-    """
-    Função de calculadora simples que realiza operações básicas.
-    """
- 
-    num1 = int(input('Digite o 1º número: '))
-    operador = input("Digite o operador (+, -, *, /): ")
-    num2 = int(input('Digite o 2º número: '))
+    
+    while True:
+        try:
+            num1 = int(input('Digite o 1º número: '))
+            operador = input("Digite o operador (+, -, *, /): ")
+            num2 = int(input('Digite o 2º número: '))
+        except ValueError:
+            print('Entre com algum valor interior e/ou use somente os operadores apropriados.')
    
 
-    # Realiza o cálculo baseado no operador
-    if operador == '+':
-        resultado = num1 + num2
-        return resultado
-    elif operador == '-':
-        resultado = num1 - num2
-        return resultado
-    elif operador == '*':
-        resultado = num1 * num2
-        return resultado
-    elif operador == '/':
-        # Tratamento para evitar divisão por zero
-        if num2 == 0:
-            print("Erro: Não é possível dividir por zero.")
-        else:
-            resultado = num1 / num2
+        # Realiza o cálculo baseado no operador
+        if operador == '+':
+            resultado = num1 + num2
             return resultado
-    else:
-        print('Operação não reconhecida.')
+        elif operador == '-':
+            resultado = num1 - num2
+            return resultado
+        elif operador == '*':
+            resultado = num1 * num2
+            return resultado
+        elif operador == '/':
+
+        # Tratamento para evitar divisão por zero
+            if num2 == 0:
+                print("Erro: Não é possível dividir por zero.")
+            else:
+                resultado = num1 / num2
+                return resultado
+            else:
+                print('Operação não reconhecida.')
 
 print('O resultado é:')
 print(calculadora())
--- exercicio_tabuada.py
+++ exercicio_tabuada.py
@@ -1,3 +1,11 @@
+'''
+2- Crie um programa em Python que:​
+Peça ao usuário um número inteiro.​
+Gere e exiba na tela a tabuada desse número de 1 a 10.​
+Utilize funções para deixar o código organizado.
+
+'''
+
 print('='*30)
 numero = int(input('Digite um númeto inteiro: '))
 print('='*30)
